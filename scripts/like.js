e81ef4909dfea0f3f94dafa87d61d10a5f231a9f
madp-alt <olurbum@gmail.com>
2026-04-22 16:44:23 +0300

    Update like.js

--- scripts/like.js
+++ scripts/like.js
@@ -7,18 +7,33 @@
 ✦ button__text — для обозначения текстового элемента внутри кнопки
 Если эти классы поменять в HTML, скрипт перестанет работать. Будьте аккуратны.
 */
+document.querySelectorAll('.card__like-button, .card__icon-button, .save-button, .modal__button').forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  });
+});
 
 const likeHeartArray = document.querySelectorAll('.like-icon');
 const likeButtonArray = document.querySelectorAll('.card__like-button');
 const iconButtonArray = document.querySelectorAll('.card__icon-button');
 
 iconButtonArray.forEach((iconButton, index) => {
-  iconButton.onclick = () =>
+  iconButton.onclick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
     toggleIsLiked(likeHeartArray[index], likeButtonArray[index]);
+    return false;
+  };
 });
 
 likeButtonArray.forEach((button, index) => {
-  button.onclick = () => toggleIsLiked(likeHeartArray[index], button);
+  button.onclick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    toggleIsLiked(likeHeartArray[index], button);
+    return false;
+  };
 });
 
 function toggleIsLiked(heart, button) {
